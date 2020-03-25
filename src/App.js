d104852e696ca1bb97546196f783dd18ef596485
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-03-24 23:34:35 -0500

    setState

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ class App extends Component {
       {fname: "Aswin", lname: "Timalsina", age: 23},
       {fname: "Rohan", lname: "Gugliya", age: 42},
       {fname: "Arohan", lname: "Timalsina", age: 39}
-    ]
+    ],
+    otherName: "Hola"
   }  
 
   switchNameHandler = ()=>{
     console.log("Button is clicked");
+    //DONOT USE THIS: this.state.person[0].fname = "Austin";
+    this.setState({
+      person:[
+      {fname: "Austin", lname: "Williams", age: 29},
+      {fname: "Shukhais", lname: "Gugliya", age: 69},
+      {fname: "Adam", lname: "Major", age: 88}
+      ]
+    })
   }
 
     render(){return (
@@ -23,6 +32,7 @@ class App extends Component {
         <Person fname={this.state.person[1].fname} lname={this.state.person[1].lname} age={this.state.person[1].age}> Wassup guys!!! </Person>
         <Person fname={this.state.person[2].fname} lname={this.state.person[2].lname} age={this.state.person[2].age} />
         <button onClick={this.switchNameHandler}>Switch</button>
+    <p>{this.state.otherName} {this.state.person[0].fname} {this.state.person[0].lname}!</p>
       </div>
   );}
 }
