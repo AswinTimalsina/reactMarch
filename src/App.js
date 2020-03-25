da87ace43c2516dfccc52d81ca682c2cc71991b7
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-03-25 01:19:21 -0500

    46. passing method references between components 1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import Person from './Person/Person'
 import './index.css';
 
-const App = props => { 
-  const [personState, setPersonState] = useState({
+class App extends Component{ 
+ 
+  state = {
     person:[
       {fname: "Aswin", lname: "Timalsina", age: 23},
       {fname: "Rohan", lname: "Gugliya", age: 42},
       {fname: "Arohan", lname: "Timalsina", age: 39}
-    ]
-    // otherName: "Hola"
-    // after the state change, otherName gets removed; it means state is replaced not overridden
-  });
-
-  // Best way is to create another useState with the value that should not be replaced
-  const [setOtherState, setOtherPersonState] = useState({
+    ],
     otherName: "Hola"
-  })
+  };
 
-  const switchNameHandler = ()=>{
+  switchNameHandler = ()=>{
     console.log("Button is clicked");
     //DONOT USE THIS: this.state.person[0].fname = "Austin";
-    setPersonState({
+    this.setState({
       person:[
       {fname: "Austin", lname: "Williams", age: 29},
       {fname: "Shukhais", lname: "Gugliya", age: 69},
@@ -31,16 +26,20 @@ const App = props => {
     })
   }
 
-
+  render(){
  return (
       <div className="box">
-        <Person fname={personState.person[0].fname} lname={personState.person[0].lname} age={personState.person[0].age} />
-        <Person fname={personState.person[1].fname} lname={personState.person[1].lname} age={personState.person[1].age}> Wassup guys!!! </Person>
-        <Person fname={personState.person[2].fname} lname={personState.person[2].lname} age={personState.person[2].age} />
-        <button onClick={switchNameHandler}>Switch</button>
-        <p>{setOtherState.otherName} {personState.person[0].fname} {personState.person[0].lname}!</p>
+        <Person fname={this.state.person[0].fname} lname={this.state.person[0].lname} age={this.state.person[0].age} />
+        <Person 
+        click={this.switchNameHandler}
+        fname={this.state.person[1].fname} lname={this.state.person[1].lname} age={this.state.person[1].age}>
+           Wassup guys!!! </Person>
+        <Person fname={this.state.person[2].fname} lname={this.state.person[2].lname} age={this.state.person[2].age} />
+        <button onClick={this.switchNameHandler}>Switch</button>
+        {/* onClick={() => this.switchNameHandler("Maximillian")} -- this can be inefficient --use bind syntax if you can*/}
+        <p>{this.state.otherName} {this.state.person[0].fname} {this.state.person[0].lname}!</p>
       </div>
-  );
+  );}
 }
 
 export default App;
