2035a6b9ca6273d42641f17b0e4ac9caeafcf4ce
Aswin Timalsina <aswin.timalsina1@gmail.com>
2020-03-26 00:33:22 -0500

    practice till now

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import logo from './logo.svg';
 import Person from './Person/Person'
 import './index.css';
 
-class App extends Component{ 
- 
+class App extends Component{
   state = {
     person:[
       {fname: "Aswin", lname: "Timalsina", age: 23},
@@ -12,35 +11,32 @@ class App extends Component{
       {fname: "Arohan", lname: "Timalsina", age: 39}
     ],
     otherName: "Hola"
-  };
+  }
 
-  switchNameHandler = (newName)=>{
-    console.log("Button is clicked");
-    //DONOT USE THIS: this.state.person[0].fname = "Austin";
+  switchNameHandler = (name) =>{
+    console.log("Button clicked");
     this.setState({
       person:[
       {fname: "Austin", lname: "Williams", age: 29},
       {fname: "Shukhais", lname: "Gugliya", age: 69},
-      {fname: newName, lname: "Major", age: 88}
+      {fname: name, lname: "Major", age: 88}
       ]
     })
   }
-
+  
+  
   render(){
- return (
-      <div className="box">
-        <Person fname={this.state.person[0].fname} lname={this.state.person[0].lname} age={this.state.person[0].age} />
-        <Person 
-        click={this.switchNameHandler.bind(this, "Austin")}
-        fname={this.state.person[1].fname} lname={this.state.person[1].lname} age={this.state.person[1].age}>
-           Wassup guys!!! </Person>
-        <Person fname={this.state.person[2].fname} lname={this.state.person[2].lname} age={this.state.person[2].age} />
-        <button onClick={this.switchNameHandler.bind(this, "Maximillian")}>Switch</button>
-        {/* onClick={() => this.switchNameHandler("Maximillian")} -- this can be inefficient --use bind syntax if you can*/}
-        <p>{this.state.otherName} {this.state.person[0].fname} {this.state.person[0].lname}!</p>
-      </div>
-  );}
-}
+return(
+  <div className="box">
+<Person fname={this.state.person[0].fname} lname={this.state.person[0].lname} age={this.state.person[0].age}/>
+<Person click={this.switchNameHandler.bind(this, "Austin")} fname={this.state.person[1].fname} lname={this.state.person[1].lname} age={this.state.person[1].age}>Wassup guys!!!</Person>
+<Person fname={this.state.person[2].fname} lname={this.state.person[2].lname} age={this.state.person[2].age}/>
+<button onClick={this.switchNameHandler.bind(this, "Maximillian")}>Click Here</button>
+<p>{this.state.otherName} {this.state.person[0].fname} {this.state.person[0].lname}!</p>
+</div>
+)
 
+  }
+}
 export default App;
 
